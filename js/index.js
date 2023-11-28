5edffab056b860d92d1088468d9e7b837c8309db
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-28 16:11:21 +0100

    Added eye color function

--- js/index.js
+++ js/index.js
@@ -2,12 +2,29 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
+// - log the entire data object
+// async function fetchData() {
+//   const response = await fetch(url);
+//   const json = await response.json();
+//   const data = json.results;
+
+//   return data;
+// }
+// fetchData().then((value) => {
+//   console.log(value);
+// });
+
+// - Bonus: Can you log the eye color of R2-D2?
 async function fetchData() {
   const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  const json = await response.json();
+  return json.results;
 }
-
-fetchData().then((value) => {
-  console.log(value);
-});
+async function main() {
+  const data = await fetchData();
+  //console.log(data[2].eye_color);
+  const result = data.filter((item) => {
+    return item.name == "Darth Vader";
+  });
+  console.log(result);
+}
